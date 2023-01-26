f7fc053eec29d7c5747ff7c75aa39f8a16f5b5c0
Erick294 <beowulfblackman@hotmail.com>
2023-01-25 19:45:51 -0500

    Taller 22

--- src/components/Estados.vue
+++ src/components/Estados.vue
@@ -1,30 +1,30 @@
 <template>
   <div id="lista_estados">
-    <li>
-        <ul v-for:="element in 55">{{element.nombre}}</ul>
-    </li>
+    <ul>
+        <li v-for:="element in listaEstados">{{element}}</li>
+    </ul>
   </div>
 
-  <input v-on:keyup.enter="obtenerResultados()" type="text" placeholder="Ingrese las iniciales del estado">
+  <input v-on:keyup.enter="buscarEstado()" v-model="codigo" type="text" placeholder="Ingrese las iniciales del estado">
 
   <div v-if="mostrar" id="resultados">
     <label for="positivos">Casos positivos:</label>
-    <input id="positivos" type="text" placeholder={{ positivos }}>
+    <input id="positivos" type="text" value={{ positivos }}>
 
     <label for="resultadosT">Resultados totales:</label>
-    <input id="resultadosT" type="text" placeholder={{ resultadosTotales }}>
+    <input id="resultadosT" type="text" value={{ resultadosTotales }}>
 
     <label for="hospitalizados">Hospitalizados:</label>
-    <input id="hospitalizados" type="text" placeholder={{ hospitalizados }}>
+    <input id="hospitalizados" type="text" value={{ hospitalizados }}>
 
     <label for="muertos">Casos positivos:</label>
-    <input id="muertos" type="text" placeholder={{ muertos }}>
+    <input id="muertos" type="text" value={{ muertos }}>
 
     <label for="totalViral">Casos positivos:</label>
-    <input id="totalViral" type="text" placeholder={{ totalViral }}>
+    <input id="totalViral" type="text" value={{ totalViral }}>
 
     <label for="incrementoMuertos">Casos positivos:</label>
-    <input id="incrementoMuertos" type="text" placeholder={{ incrementoMuertos }}>
+    <input id="incrementoMuertos" type="text" value={{ incrementoMuertos }}>
  
   </div>
 </template>
@@ -34,7 +34,8 @@ export default {
     data(){
         return{
             listaEstados:[],
-            codigo: "CA",
+            listaDatosEstados:[],
+            codigo: "",
             resultadosTotales: null,
             hospitalizados: null,
             muertos: null,
@@ -43,52 +44,76 @@ export default {
             mostrar: false
         }
     },
-    mounted:{
+    mounted(){
+        this.obtenerData();
+        console.log("Mi componente se monto")
+    },
+    methods:{
         async obtenerApi(){
             const data = await fetch(`https://api.covidtracking.com/v1/states/current.json`).then((r) => r.json());
             return data;
-        }
-    },
-    methods:{
-        async obtenerApiCodigo(codigo){
-            const data = await fetch(`https://api.covidtracking.com/v1/states/${codigo}/current.json`).then((r) => r.json());
-            return data;
         },
         async obtenerData(){
-            const data = await obtenerApi()
+            const data = await this.obtenerApi()
             for(let i=0; i<55; i++){
-                this.listaEstados.unshift(data[i])
+                const datos = data[i]
+                const {state, positive, totalTestResults, hospitalizedCurrently, death,
+                                                        totalTestViral, deathIncrease} = datos
+                const objetoEstado = {
+                    state: state,
+                    positivos: positive,
+                    totalTestResults: totalTestResults,
+                    hospitalizedCurrently: hospitalizedCurrently,
+                    death: death,
+                    totalTestViral:totalTestViral,
+                    deathIncrease:deathIncrease
+                }                                      
+                this.listaEstados.unshift(state);
+                this.listaDatosEstados.unshift(objetoEstado);
             }
         },
-        async construirObjetoEstado(codigo){
-                const {state, positive, totalTestResults, hospitalizedCurrently, death,
-                            totalTestViral, deathIncrease} = await this.obtenerApiCodigo(codigo);
-
-                for(let i=0; i<55; i++){
-                    const objetoEstado = {
-                        nombre: state,
-                        positivos: positive,
-                        resultadosTotales: totalTestResults,
-                        hospitalizados: hospitalizedCurrently,
-                        muertos: death,
-                        totalViral: totalTestViral,
-                        incrementoMuertes: deathIncrease
-                    };
-                    return objetoEstado;  
-                }  
-                  
-            },
-            obtenerResultados(){
-                const data = this.construirObjetoEstado(this.codigo.toLowerCase());
-                this.mostrar = true
-                this.positivos = data.positivos
-                this.resultadosTotales = data.resultadosTotales
-                this.hospitalizados = data.hospitalizados
-                this.muertos = data.muertos
-                this.totalViral = data.totalViral
-                this.incrementoMuertes = data.incrementoMuertes
+        async buscarEstado(){
+            console.log(this.listaDatosEstados)
+            for (let i=0; i<55; i++){
+                const aux = this.listaDatosEstados[i]
+                if(this.codigo.includes(aux.state)){
+                    this.mostrar = true
+                    this.resultadosTotales = aux.totalTestResults
+                    this.hospitalizados = aux.hospitalizedCurrently
+                    this.muertos = aux.death
+                    this.totalViral = aux.totalTestViral
+                    this.incrementoMuertes = aux.deathIncrease
+                }else{
+                    console.log(aux.state)
+                }
             }
+                 
+        }      
+    },
+    renderTracked(){
+        console.log("renderTracked")
+    },
+    renderTriggered(){
+        console.log("renderTriggered")
+    },unmounted(){
+        console.log("unmounted")
+    },
+    created(){
+        console.log("created")
+    },
+    beforeCreate(){
+        console.log("beforeCreated")
+    },
+    beforeMount(){
+        console.log("beforeMount")
+    },
+    beforeUpdate(){
+        console.log("beforeUpdate")
+    },
+    updated(){
+        console.log("updated")
     }
+
 }
 </script>
 
@@ -96,4 +121,20 @@ export default {
 template{
     text-align: center;
 }
+
+#lista_estados{
+    height: 455px;
+    width: 100px;
+}
+
+ul{
+    column-count: 4;
+    height: 455px;
+    width: 1300px;
+}
+
+li{
+    padding: 5px;
+}
+
 </style>
